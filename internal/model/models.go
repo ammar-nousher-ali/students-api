a1a753c444a9041576031d18b10847417e7fdbd7
ammar <ammarnousher56@gmail.com>
2025-07-30 01:02:26 +0500

    added email validation on student email while adding new student

--- internal/model/models.go
+++ internal/model/models.go
@@ -3,7 +3,7 @@ package model
 type Student struct {
 	Id    int64  `json:"id"`
 	Name  string `json:"name" validate:"required"`
-	Email string `json:"email" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
 	Age   int    `json:"age" validate:"required"`
 }
 
